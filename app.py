b723f5b0fa748ec1c87a930bd27aaa853e9ee4c0
canertunc <canertun982@gmail.com>
2024-05-15 21:51:25 +0300

    update commit

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
-# from flask import Flask, request, jsonify, render_template
-# import spamFilter  # Modelinizi içeren modülü buraya ekleyin
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/scan', methods=['POST'])
-# def scan_email():
-#     email_content = request.form['email_content']
-#     # Modelinizi kullanarak burada e-posta içeriğini işleyin
-#     result = spamFilter.is_spam(email_content)
-#     return jsonify({'result': result})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 
 from flask import Flask, request, jsonify, render_template
-import spamFilter  # Modelinizi içeren modülü buraya ekleyin
-
+import spamFilter
+import csv  
 app = Flask(__name__)
 
 @app.route('/mainMenu')
@@ -29,7 +10,7 @@ def main_menu():
 
 @app.route('/generalMode')
 def index():
-    return render_template('generalMode.html')  # İlk açılış sayfanız
+    return render_template('generalMode.html')  
 
 @app.route('/selectmode')
 def select_mode():
@@ -49,7 +30,53 @@ def scan_email():
     email_content = request.form['email_content']
     result = spamFilter.is_spam(email_content)
     print(type(result))
-    return render_template('generalMode.html', result=result)  # Sonuç sayfanızı render edin
+    return render_template('generalMode.html', result=result)  
+
+@app.route('/update_black_list', methods=['POST'])
+def update_black_list():
+
+    with open('black_list.csv', 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+    print("go1")
+
+    word = request.form['add']
+    rows.append([word, 100.0])
+
+    with open('black_list.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+
+    return render_template('lists.html')
+
+@app.route('/update_white_list', methods=['POST'])
+def update_white_list():
+
+    with open('white_list.csv', 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+
+    word = request.form['remove']
+    rows.append([word, -100.0])
+
+    with open('white_list.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+
+    
+
+    with open('black_list.csv', 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+
+    new_rows = [row for row in rows if row[0] != word]
+
+    with open('black_list.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(new_rows)
+
+
+    return render_template('lists.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
